9a1ddc595a1ee6cc9e9102aae91c3e331bce241f
Channy <chthonic3719@gmail.com>
2021-05-15 17:50:01 +0800

    week3hw.done

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -12,3 +12,35 @@ function solve(lines) {
   }
 }
 solve(['5 200'])
+
+/*
+數學解法
+
+function solve(lines) {
+  let arr = lines[0].split(' ')
+  for (let i = Number(arr[0]); i <= Number(arr[1]); i++) {
+   isNarcissisticNumber(i, digitsCount(i))
+  }
+}
+
+function digitsCount (n) {
+  let result = 0
+  while (n != 0) {
+    n = Math.floor(n / 10)
+    result++
+  }
+  return result
+}
+
+function isNarcissisticNumber (num, digits) {
+  let n = num
+  let sum = 0
+  while (n != 0) {
+    sum += (n % 10) ** digits
+    n = Math.floor(n / 10)
+  }
+  if (sum === num) {
+    console.log(num)
+  }
+}
+*/
